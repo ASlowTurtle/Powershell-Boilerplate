e86e9736199157accfb1a90951105b4cbbedcfdc
anders <anders@fdp.wtf>
2024-09-04 23:29:04 +0200

    Get-PnpDevice

--- Get-InstalledUSBStorageDevices.ps1
+++ Get-InstalledUSBStorageDevices.ps1
@@ -7,7 +7,7 @@ $USBDevices = Get-ChildItem -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\USB"
 foreach ($USBDevice in $USBDevices) {
     $USBDeviceType = get-childitem -path "Registry::$($USBDevice.Name)"
     foreach ($SingleDevice in $USBDeviceType) {
-        if ($SingleDevice.GetValue("DeviceDesc") -match "usbstor") {
+        if ($SingleDevice.GetValue("DeviceDesc") -match "(usbstor|disk)") {
             $SingleDevice.Name.replace("HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Enum\","")
         }
     }
@@ -16,3 +16,6 @@ foreach ($USBDevice in $USBDevices) {
 
 # pnputil /remove-device "USB\VID_0951&PID_1666\E0D55EA574A8E420E8374D4C"
 # pnputil /enum-devices | select-string -Pattern "SWD\\WPDBUSENUM"
+
+# Enumerating using Get-PnpDevice
+$USBStorageDevices = Get-PnpDevice -Class "USB" | Where-Object {$_.FriendlyName -like "USB Mass Storage Device*"} | Select-Object -ExpandProperty "InstanceID"
