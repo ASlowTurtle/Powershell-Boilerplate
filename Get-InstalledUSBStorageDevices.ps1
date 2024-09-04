e4b6fe7766b69c7b04dbbed6a13db899a808b253
anders <anders@fdp.wtf>
2024-09-04 22:38:58 +0200

    USBstorageDevices

---
+++ Get-InstalledUSBStorageDevices.ps1
@@ -0,0 +1,17 @@
+<#
+    Retrieves the instance name of all installed usb storage devices from the registry.
+    The instance name can be used with pnputil to uninstall the device.
+#>
+
+$USBDevices = Get-ChildItem -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\USB"
+foreach ($USBDevice in $USBDevices) {
+    $USBDeviceType = get-childitem -path "Registry::$($USBDevice.Name)"
+    foreach ($SingleDevice in $USBDeviceType) {
+        if ($SingleDevice.GetValue("DeviceDesc") -match "usbstor") {
+            $SingleDevice.Name.replace("HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Enum\","")
+        }
+    }
+}
+
+
+# pnputil /remove-device "USB\VID_0951&PID_1666\E0D55EA574A8E420E8374D4C"
