c793b184aa5d12769881c40e2f7300f8efe1122a
anders <anders@fdp.wtf>
2024-09-10 21:36:10 +0200

    Remove devices

--- Get-InstalledUSBStorageDevices.ps1
+++ Get-InstalledUSBStorageDevices.ps1
@@ -20,3 +20,44 @@ foreach ($USBDevice in $USBDevices) {
 # Enumerating using Get-PnpDevice
 $USBStorageDevices = Get-PnpDevice -Class "USB" | Where-Object {$_.FriendlyName -like "USB Mass Storage Device*"} | Select-Object -ExpandProperty "InstanceID"
 $USBStorageDevices = Get-PnpDevice -FriendlyName "USB Mass Storage Device" | Select-Object -ExpandProperty InstanceID
+
+
+
+$ListOnly = $false
+$ContainerIDs = @()
+$DeviceKeys = @()
+$DevicesToUninstall = @()
+$USBDevices = Get-ChildItem -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\USB"
+foreach ($USBDevice in $USBDevices) {
+    $USBDeviceType = get-childitem -path "Registry::$($USBDevice.Name)"
+    foreach ($SingleDevice in $USBDeviceType) {
+        if ($SingleDevice.GetValue("DeviceDesc") -match "(usbstor|disk)") {
+            $DevicesToUninstall += $SingleDevice.Name.replace("HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Enum\","")
+            $ContainerIDs += $SingleDevice.GetValue("ContainerID")
+        }
+    }
+}
+foreach ($ContainerID in $ContainerIDs) {
+    $DeviceKeys += Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\DeviceContainers\$ContainerID\BaseContainers\$ContainerID" | Get-Member -MemberType NoteProperty | Where-Object "Name" -Notmatch "PS.*"
+}
+$DeviceKeys = $DeviceKeys.Name
+
+foreach ($DeviceToUninstall in $DevicesToUninstall) {
+    Write-Host "$DeviceToUninstall"
+    if (-not $ListOnly) {
+        pnputil.exe /disable-device $DeviceToUninstall
+        pnputil.exe /remove-device $DeviceToUninstall
+    }
+    
+}
+foreach ($DeviceKey in $DeviceKeys) {
+    Write-Host "HKLM:\SYSTEM\CurrentControlSet\Enum\$DeviceKey"
+    if (Test-Path -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$DeviceKey") {
+        Write-Host "Found regkey."
+        if (-not $ListOnly) {
+            Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$DeviceKey" -Force -Recurse
+        }
+    } else {
+        Write-Host "Regkey not found."
+    }
+}
